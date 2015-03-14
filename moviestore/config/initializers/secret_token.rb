fe5405e5cf6c572add69f2d459003744cc2cb541
lhtrieu87 <lhtrieu87@gmail.com>
2015-03-14 12:06:01 +0800

    Initial set up with movie scaffolding

---
+++ moviestore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviestore::Application.config.secret_key_base = 'dfb1d230e03b417ff784375fd977da8478144132966489dcf5913e6052232cb80b47198ecc85200c85ad43f3a139e339943d4f57b5c51694c80a127d65e05370'
---
+++ moviestore/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moviestore::Application.config.session_store :cookie_store, key: '_moviestore_session'
