ce5d0e8a43e5e6103455dde562417ed905960d72
lhtrieu87 <lhtrieu87@gmail.com>
2015-03-14 22:47:07 +0800

    root 'movies#index'
    - Ruby symbols are immutable in run time, which can be used as strings if they are unchanged in run time. They save memory as all appearances of the same symbol string name refer to the same memory. Symbol comparison is also fast as it performs object id comparison instead of comparison on two sequences of character(s).

--- moviestore/config/initializers/session_store.rb
+++ moviestore/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Moviestore::Application.config.session_store :cookie_store, key: '_moviestore_session'
+Rails.application.config.session_store :cookie_store, key: '_moviestore_session'
