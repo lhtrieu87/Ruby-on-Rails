d046c24871af31e91857b7e54583d62288b2f2ba
lhtrieu87 <lhtrieu87@gmail.com>
2015-03-16 00:16:28 +0800

    Cart
    - Singular resources can be looked up without an id as they are implicitly associated with the current session

--- moviestore-tr/config/routes.rb
+++ moviestore-tr/config/routes.rb
@@ -1,4 +1,5 @@
 Moviestore::Application.routes.draw do
+  get "carts/show"
   devise_for :users, path_names: { sign_in: :login, sign_out: :logout, sign_up: :register }
   resources :movies, only: [:show, :index]
 
@@ -7,6 +8,11 @@ Moviestore::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
+
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
